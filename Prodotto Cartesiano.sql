e1f142e16072de3c4066bbbb2ecd940b30c77240
Antoc00 <acemail02@gmail.com>
2024-03-21 11:05:39 +0100

    Funzione prodotto cartesiano

--- Prodotto Cartesiano.sql
+++ Prodotto Cartesiano.sql
@@ -1,5 +1,6 @@
 select reset();
 select POPOLAMENTO(10000, 0.1, 0.1);
+
 CREATE INDEX idx_t1_s1 ON t1 USING GIST (s1);
 CREATE INDEX idx_t2_s1 ON t2 USING GIST (s1);
 CREATE INDEX idx_t1_d1 ON t1 USING GIST (d1);
@@ -13,34 +14,36 @@ CREATE INDEX idx_t2_s3 ON t2 USING GIST (s3);
 CREATE INDEX idx_t1_d3 ON t1 USING GIST (d3);
 CREATE INDEX idx_t2_d3 ON t2 USING GIST (d3);
 
-
-/* CON PYRAMID INTERSECT
-CREATE OR REPLACE FUNCTION pyramid_intersect(
-    s1_t1 int4range, d1_t1 int4range, s2_t1 int4range, d2_t1 int4range, s3_t1 int4range, d3_t1 int4range,
-    s1_t2 int4range, d1_t2 int4range, s2_t2 int4range, d2_t2 int4range, s3_t2 int4range, d3_t2 int4range
-)
-RETURNS TABLE(s1 int4range, d1 int4range, s2 int4range, d2 int4range, s3 int4range, d3 int4range) AS
-$$
+CREATE OR REPLACE FUNCTION prodotto_cartesiano()
+RETURNS TABLE(
+  Attr1_t1 varchar(150),
+  Attr1_t2 varchar(150),
+  Attr2_t1 varchar(150),
+  Attr2_t2 varchar(150),
+  s1 int4range,
+  d1 int4range,
+  s2 int4range,
+  d2 int4range,
+  s3 int4range,
+  d3 int4range ) AS $$
 BEGIN
-    RETURN QUERY SELECT
-        s1_t1 * s1_t2,
-        d1_t1 * d1_t2,
-        s2_t1 * s2_t2,
-        d2_t1 * d2_t2,
-        s3_t1 * s3_t2,
-        d3_t1 * d3_t2;
+RETURN QUERY
+SELECT 
+  t1.Attr1, t2.Attr1, t1.Attr2, t2.Attr2,
+  t1.s1 * t2.s1, t1.d1 * t2.d1,
+  t1.s2 * t2.s2, t1.d2 * t2.d2,
+  t1.s3 * t2.s3, t1.d3 * t2.d3
+FROM 
+  t1, t2 
+WHERE 
+  t1.s1 && t2.s1 AND 
+  t1.d1 && t2.d1;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
-select t1.Attr1, t2.Attr1, t1.Attr2 , t2.Attr2, pyramid_intersect(t1.s1, t1.d1, t1.s2, t1.d2, t1.s3, t1.d3,t2.s1, t2.d1, t2.s2, t2.d2, t2.s3, t2.d3)
-from t1, t2 
-where t1.s1 && t2.s1 and t1.d1 && t2.d1
-*/
+SELECT * FROM prodotto_cartesiano();
 
-/* SENZA PYRAMID INTERSECT*/
-select t1.Attr1, t2.Attr1, t1.Attr2 , t2.Attr2, t1.s1 * t2.s1 , t1.d1 * t2.d1, t1.s2 * t2.s2 , t1.d2 * t2.d2, t1.s3 * t2.s3 , t1.d3 * t2.d3
-from t1, t2 
-where t1.s1 && t2.s1 and t1.d1 && t2.d1
 
   
   
